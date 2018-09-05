5aba49887cd45db013bc893efeb3666c560d7445
Ezequiel <e_taiko@hotmail.com>
2018-09-05 17:39:41 -0300

    seteo de volley en rama de prueba

--- app/src/main/java/com/fernet/ezequielmatiaspelozo/sharebook/Libros.java
+++ app/src/main/java/com/fernet/ezequielmatiaspelozo/sharebook/Libros.java
@@ -4,11 +4,20 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class Libros extends Activity {
+
+    private RequestQueue mRequestQueue;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_libros);
+
+        mRequestQueue = Volley.newRequestQueue(this);
     }
 }

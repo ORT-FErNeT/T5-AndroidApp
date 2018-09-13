a9aecca071493db4eb109d42d24cb2c1cc808be8
Ezequiel <e_taiko@hotmail.com>
2018-09-13 18:13:47 -0300

    cambios esteticos1

--- app/src/main/java/com/fernet/ezequielmatiaspelozo/sharebook/MainActivity.java
+++ app/src/main/java/com/fernet/ezequielmatiaspelozo/sharebook/MainActivity.java
@@ -9,19 +9,30 @@ import android.widget.Button;
 public class MainActivity extends Activity {
 
     private Button inicio;
+    private Button buscar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        inicio = (Button) findViewById(R.id.boton1);
+        inicio = (Button) findViewById(R.id.boton_menu);
+        buscar = (Button) findViewById(R.id.boton_buscar);
 
         inicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // first parameter is the context, second is the class of the activity to launch
                 Intent i = new Intent(getApplicationContext(), Menu.class);
+                startActivity(i); // brings up the second activity
+            }
+        });
+
+        buscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // first parameter is the context, second is the class of the activity to launch
+                Intent i = new Intent(getApplicationContext(), ApiConnect.class);
                 startActivity(i); // brings up the second activity
             }
         });

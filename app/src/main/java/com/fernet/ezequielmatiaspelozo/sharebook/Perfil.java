7ebd1699022c98c8b9d22824d778ec0dbf816f9c
Ezequiel <e_taiko@hotmail.com>
2018-09-23 15:12:36 -0300

    adicion de boton de modificar perfil

--- app/src/main/java/com/fernet/ezequielmatiaspelozo/sharebook/Perfil.java
+++ app/src/main/java/com/fernet/ezequielmatiaspelozo/sharebook/Perfil.java
@@ -2,17 +2,21 @@ package com.fernet.ezequielmatiaspelozo.sharebook;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class Perfil extends Activity {
 
     private ImageView perfil;
+    private Button editarPerfilButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        editarPerfilButton = (Button) findViewById(R.id.boton_editar_perfil);
 
         perfil = (ImageView) findViewById(R.id.imagen_perfil);
         perfil.setImageResource(R.drawable.avatar);

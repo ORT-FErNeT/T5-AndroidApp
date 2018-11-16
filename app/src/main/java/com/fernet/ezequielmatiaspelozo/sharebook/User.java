cbbe2a82c391ebf9b5f973ac38e58f0d432afa66
Ezequiel <e_taiko@hotmail.com>
2018-11-16 20:33:48 -0300

    cambios en user y editperfil

--- app/src/main/java/com/fernet/ezequielmatiaspelozo/sharebook/User.java
+++ app/src/main/java/com/fernet/ezequielmatiaspelozo/sharebook/User.java
@@ -7,16 +7,20 @@ public class User {
     public String edad;
     public String ubicacion;
     public String preferencias;
+    public String usserName;
+    public String password;
 
     public User(){
 
     }
 
-    public User(String nombre, String apellido, String edad, String ubicacion, String preferencias) {
+    public User(String nombre, String apellido, String edad, String ubicacion, String preferencias, String usserName, String password) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
         this.ubicacion = ubicacion;
         this.preferencias = preferencias;
+        this.usserName = usserName;
+        this.password = password;
     }
 }
